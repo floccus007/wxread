e95aa6cf29b48579aace2ed40baa442087dca52d
floccus007 <lncool2008@qq.com>
2025-04-11 00:22:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import hashlib
 import requests
 import urllib.parse
 from push import push
-from config import data, headers, cookies, READ_NUM, PUSH_METHOD
+from config import data, headers, cookies, READ_NUM, PUSH_METHOD, book, chapter
 
 # 配置日志格式
 logger = logging.getLogger(__name__)
@@ -51,10 +51,18 @@ def get_wr_skey():
     return None
 
 
+
+
 index = 1
+lastTime = int(time.time()) - 30
 while index <= READ_NUM:
-    data['ct'] = int(time.time())
-    data['ts'] = int(time.time() * 1000)
+    data['b'] = random.choice(book)
+    data['c'] = random.choice(chapter)
+    thisTime = int(time.time())
+    data['ct'] = thisTime
+    data['rt'] = thisTime - lastTime
+    lastTime = thisTime
+    data['ts'] = int(thisTime * 1000) + random.randint(0, 1000)
     data['rn'] = random.randint(0, 1000)
     data['sg'] = hashlib.sha256(f"{data['ts']}{data['rn']}{KEY}".encode()).hexdigest()
     data['s'] = cal_hash(encode_data(data))
